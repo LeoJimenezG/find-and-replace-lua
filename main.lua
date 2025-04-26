874555472f0726c6548da3e7ab8873a4da989647
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-04-26 02:03:52 -0600

    update main code

--- main.lua
+++ main.lua
@@ -1,13 +1,11 @@
-local welcome_message = "\n>>>>> WARNING: Make sure to paste your text into the 'data.txt' file before doing anything else, and, in the first line paste the element to find! <<<<<\n"
+local welcome_message = "\n>>>>> WARNING: Make sure the 'data.txt' file is properly formatted. For more information go to the 'README.md' file! <<<<<\n"
 
 local options = [[
 --------------- AVAILABLE OPTIONS ---------------
 1. Replace all occurrences found
-2. Replace all occurrences from a starting position to the end
-3. Replace all occurrences from the start to an ending position
-4. Count all occurrences
-5. Replace and Count all occurrences
-6. Exit
+2. Count all occurrences
+3. Replace and Count all occurrences
+4. Exit
 ]]
 
 local function show_options ()
@@ -16,37 +14,116 @@ end
 
 local function get_file_content ()
     local file = io.open("data.txt", "r")
+
     if file ~= nil then
         local file_lines = {}
         for line in file:lines() do
             table.insert(file_lines, line)
         end
         file:close()
+
         return file_lines
     end
+
     return nil
 end
 
-local function ask_user_option (element)
+local function ask_user_option ()
     show_options()
     local option = tonumber(io.read())
-    if option > 0 and option < 7 then
-        print("-> You selected the option number: " .. "'" .. tostring(option) .. "'\n")
+
+    if option > 0 and option < 5 then
         return option
     else
-        return ask_user_option(element)
+        return ask_user_option()
     end
+end
+
+local function write_new_file (new_content)
+    local output = io.open("output.txt", "w")
+
+    if output == nil then
+        print("\n>>>>> Error: The 'output.txt' file was not found! <<<<<\n")
+        return false
+    end
+
+    for i = 1, #new_content do
+        output:write(new_content[i])
+        output:write("\n")
+    end
+    output:close()
+
+    return true
+end
+
+local function replace_all_occurrences (file_content)
+    local element_to_find = file_content[1]
+    local element_to_use = file_content[2]
+    local new_content = {}
+
+    for i = 3, #file_content do
+        local new_line, _ = string.gsub(file_content[i], element_to_find, element_to_use)
+        table.insert(new_content, new_line)
+    end
+
+    return new_content
+end
+
+local function count_all_ocurrences (file_content)
+    local element = file_content[1]
+    local total_occurrences = 0
+
+    for i = 3, #file_content do
+        local _, occurrences = string.gsub(file_content[i], element, element)
+        total_occurrences = total_occurrences + occurrences
+    end
+
+    return total_occurrences
+end
+
+local function replace_and_count_all (file_content)
+    local element_to_find = file_content[1]
+    local element_to_use = file_content[2]
+    local total_occurrences = 0
+    local new_content = {}
+
+    for i = 3, #file_content do
+        local new_line, occurrences = string.gsub(file_content[i], element_to_find, element_to_use)
+        total_occurrences = total_occurrences + occurrences
+        table.insert(new_content, new_line)
+    end
+
+    return new_content, total_occurrences
 end
 
 local function main()
     local file_data = get_file_content()
+
     if file_data ~= nil then
-        local element = file_data[1]
-        print("-> This is the element to be used: " .. "'" .. tostring(element) .. "'\n")
-        local user_option = ask_user_option(element)
+        print("NOTE: This is the element to be found: " .. "'" .. tostring(file_data[1]) .. "'")
+        print("NOTE: This is the element to be used: " .. "'" .. tostring(file_data[2]) .. "'\n")
+
+        local user_option = ask_user_option()
+
+        if user_option == 1 then
+            local new_content = replace_all_occurrences(file_data)
+            print("->> All ocurrences replaced! <<-\n")
+            write_new_file(new_content)
+        elseif user_option == 2 then
+            local total_occurrences = count_all_ocurrences(file_data)
+            print("->> Total occurrences found: " .. total_occurrences .. " <<-\n")
+        elseif user_option == 3 then
+            local new_content, total_occurrences = replace_and_count_all(file_data)
+            print("->> All ocurrences replaced! <<-")
+            print("->> Total occurrences found: " .. total_occurrences .. " <<-\n")
+            write_new_file(new_content)
+        else
+            print("->> Bye! <<-\n")
+        end
+    else
+        print("\n>>>>> Error: No file content provided! <<<<<\n")
     end
 end
-
 
 print(welcome_message)
 main()
